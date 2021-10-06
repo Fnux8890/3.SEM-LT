b48dd1e7d19cd346708ce7a65777791812711c2b
Sebastian <sechr17@student.sdu.dk>
2021-10-06 12:03:14 +0200

    Controllers færdiggjort

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const express = require("express") 
+const express = require('express')
 const app = express()
-const connectDB = require("./app/db/connect")
-const routes = require("./app/controller/routes/routes")
+const connectDB = require('./app/db/connect')
+const routes = require('./app/controller/routes/routes')
 
-const gate = 3000;
+const gate = 3000
 
 //middleware
 app.use(express.json())
 
 //routes
-app.use("/api/v1/users",routes)
+app.use('/api/v1/users', routes)
 
 const start = async () => {
-    try {
-        await connectDB()
-        app.listen(gate, console.log(`server listening at gate ${gate}...`))
-    } 
-    catch (error) {
-        console.log(error)
-    }
+	try {
+		await connectDB()
+		app.listen(gate, console.log(`server listening at gate ${gate}...`))
+	} catch (error) {
+		console.log(error)
+	}
 }
 
 start()
--- app/db/connect.js
+++ app/db/connect.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 const connectDB = () => {
-    const url = 'mongodb://127.0.0.1:27017/Users'; 
-    (async () => {
-        try {
-            await mongoose.connect(url);
-            console.log(`MongoDB Connected: ${url}`);
-        } catch (err) {
-            console.error(err);
-        }
-    })();
+	const url = 'mongodb://127.0.0.1:27017/Users'
+	;(async () => {
+		try {
+			await mongoose.connect(url)
+			console.log(`MongoDB Connected: ${url}`)
+		} catch (err) {
+			console.error(err)
+		}
+	})()
 }
 
 module.exports = connectDB
