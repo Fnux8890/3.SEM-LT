a54296106717c81b9b729833cb93a8606772fb0a
Sebastian <sechr17@student.sdu.dk>
2021-10-16 06:27:11 +0200

    added jquery

--- app/views/login.js
+++ app/views/login.js
@@ -1,24 +1,42 @@
-params = window.location.search
-const id = new URLSearchParams(params).get('id')
+//params = window.location.search
+
+//const id = new URLSearchParams(params).get('id')
 let tempName
-console.log(id)
+//console.log(id)
+$(() => {
+const GetAllUsers = async() => {
+try {
+const {
+	data: { users },
+} = await axios.get('/api/v1/users');
+console.log(users)
+
+$.getJSON('/api/v1/users',(data)=> {
+	console.log(data);
+
+})
+
+
+const getUsers = users.map((user) => {
+	const { _id: userID, username, password } = user
+	console.log(user)
+}).join('')
+}
+catch(error) {
+	console.log(error)
+}
+}
+GetAllUsers()
 
 document.addEventListener('submit', async e => {
 	e.preventDefault()
 	const name = document.getElementById('username').value
 
-	console.log(name)
-	console.log(id)
+	console.log(name)	
 
 	try {
-		const {
-			data: { user },
-		} = await axios.get(`/api/v1/users/${id}`)
-		const { _id: userID, username, password } = user
 
-		tempName = username
-
-		if ((name = tempName)) {
+		if ((name == tempName)) {
 			console.log('user exists')
 			window.open('http://localhost:3000/user-overview', '_parent')
 		} else {
@@ -27,4 +45,5 @@ document.addEventListener('submit', async e => {
 	} catch (error) {
 		console.log(error)
 	}
+})
 })
