55d01c812f34a0a211e7123a0a1e3bbd72bcbc3d
Sebastian <sechr17@student.sdu.dk>
2021-10-19 07:47:05 +0200

    added front end til login route

--- app/views/login.js
+++ app/views/login.js
@@ -1,4 +1,16 @@
 $(() => {
+	const element = document.getElementById('usernameerror')
+
+	function loginError() {
+		element.style.fontSize = '14px'
+		element.style.position = 'fixed'
+		element.style.top = '180px'
+		element.style.left = '60px'
+		element.style.visibility = 'visible'
+		element.textContent = 'Incorrect username or password'
+		console.log('usernameerror')
+	}
+
 	document.addEventListener('submit', async e => {
 		e.preventDefault()
 		try {
@@ -6,10 +18,23 @@ $(() => {
 			password = document.getElementById('password').value
 			console.log(username)
 			console.log(password)
-			await axios.post('/api/v1/users/login', username, password)
-			//window.open('http://localhost:3000/user-overview', '_parent')
+			const auth = await axios.post('/api/v1/users/login', {
+				username,
+				password,
+			})
+
+			if (auth) {
+				console.log('login succesful')
+				window.open('http://localhost:3000/user-overview', '_parent')
+			} else {
+				loginError()
+			}
 		} catch (error) {
 			console.log(error)
+			loginError()
+			setTimeout(() => {
+				element.style.visibility = 'hidden'
+			}, 5000)
 		}
 	})
 })
