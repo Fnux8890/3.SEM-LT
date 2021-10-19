8b3aa5db38afac99b7e5dc81b130e314af45baa4
Sebastian <sechr17@student.sdu.dk>
2021-10-19 12:52:08 +0200

    Fixes

--- app.js
+++ app.js
@@ -1,57 +1,51 @@
-const express = require("express");
-const app = express();
-const connectDB = require("./app/db/connect");
-const path = require("path");
-const fs = require("fs");
-const insertNavbar = require("./app/controller/middleware/insertNavbar");
+const express = require('express')
+const app = express()
+const connectDB = require('./app/db/connect')
+const path = require('path')
+const fs = require('fs')
+const insertNavbar = require('./app/controller/middleware/insertNavbar')
 // require files
-const userRoutes = require("./app/controller/routes/users");
-const pageRoutes = require("./app/controller/routes/page");
+const userRoutes = require('./app/controller/routes/users')
+const pageRoutes = require('./app/controller/routes/pages')
 // port
-const port = 3000;
+const port = 3000
 
 //Load view enigne
-app.set("views", path.join(__dirname, "app", "views", "pages"));
-app.set("view engine", "pug");
-
-//view engine
-app.set('views', path.join(__dirname, '/app/views'))
+app.set('views', path.join(__dirname, 'app', 'views', 'pages'))
 app.set('view engine', 'pug')
 
 //middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/page/*", insertNavbar);
-
-app.use(express.static('./app/views'))
+app.use(express.static(path.join(__dirname, 'app', 'views')))
 app.use(express.json()) //Kan se JSON payloads fra front-end
 app.use(express.urlencoded({ extended: false })) //Kan se String/text payloads fra front-end
+app.use('/page/*', insertNavbar)
 
 //routes
-app.use("/api/v1/users", userRoutes);
-app.use("/page", pageRoutes);
+app.use('/api/v1/users', userRoutes)
+app.use('/page', pageRoutes)
 
 //error handler
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+	console.log(err.message)
+	res.status(err.status || 500)
 
-  res.render("error", {
-    errorCode: err.status,
-    errorMessage: err.message,
-  });
-});
+	res.render('error', {
+		errorCode: err.status,
+		errorMessage: err.message,
+	})
+})
 
 //server setup
 const start = () => {
-  try {
-    //TODO man burde ikke connecte til databasen med det samme
-    //connectDB();
-    app.listen(
-      port,
-      console.log(`server listening at: http://localhost:${port}`)
-    );
-  } catch (error) {
-    console.log(error.message);
-  }
-};
-start();
+	try {
+		//TODO man burde ikke connecte til databasen med det samme
+		connectDB()
+		app.listen(
+			port,
+			console.log(`server listening at: http://localhost:${port}`)
+		)
+	} catch (error) {
+		console.log(error.message)
+	}
+}
+start()
--- app/controller/users/users.js
+++ app/controller/users/users.js
@@ -1,16 +1,16 @@
-const users = require("../../models/users");
+const users = require('../../models/users')
 
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const getAllUsers = async (req, res) => {
-  try {
-    const user = await users.find({});
-    res.status(200).json({ user });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+	try {
+		const user = await users.find({})
+		res.status(200).json({ user })
+	} catch (error) {
+		res.status(500).json({ msg: error })
+	}
+}
 
 const createUser = async (req, res) => {
 	try {
@@ -44,46 +44,46 @@ const createUser = async (req, res) => {
 }
 
 const getUser = async (req, res) => {
-  try {
-    const { id: userID } = req.params;
-    const user = await users.findOne({ _id: userID });
-    if (!user) {
-      return res.status(404).json({ msg: `No user with id: ${userID}}` });
-    }
-    res.status(200).json({ user });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+	try {
+		const { id: userID } = req.params
+		const user = await users.findOne({ _id: userID })
+		if (!user) {
+			return res.status(404).json({ msg: `No user with id: ${userID}}` })
+		}
+		res.status(200).json({ user })
+	} catch (error) {
+		res.status(500).json({ msg: error })
+	}
+}
 
 const updateUser = async (req, res) => {
-  try {
-    const { id: userID } = req.params;
-    const user = await users.findOneAndUpdate({ _id: userID }, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!user) {
-      return res.status(404).json({ msg: `No user with id: ${userID}}` });
-    }
-    res.status(200).json({ user });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+	try {
+		const { id: userID } = req.params
+		const user = await users.findOneAndUpdate({ _id: userID }, req.body, {
+			new: true,
+			runValidators: true,
+		})
+		if (!user) {
+			return res.status(404).json({ msg: `No user with id: ${userID}}` })
+		}
+		res.status(200).json({ user })
+	} catch (error) {
+		res.status(500).json({ msg: error })
+	}
+}
 
 const deleteUser = async (req, res) => {
-  try {
-    const { id: userID } = req.params;
-    const user = await users.findOneAndDelete({ _id: userID });
-    if (!user) {
-      return res.status(404).json({ msg: `No user with id: ${userID}}` });
-    }
-    res.status(200).json({ user });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+	try {
+		const { id: userID } = req.params
+		const user = await users.findOneAndDelete({ _id: userID })
+		if (!user) {
+			return res.status(404).json({ msg: `No user with id: ${userID}}` })
+		}
+		res.status(200).json({ user })
+	} catch (error) {
+		res.status(500).json({ msg: error })
+	}
+}
 
 const loginUser = async (req, res, next) => {
 	try {
