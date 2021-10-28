475126186a2405b2d755f6588b0ef92404995571
Sebastian <sechr17@student.sdu.dk>
2021-10-28 07:49:52 +0200

    more css(check description)
    Bare så jeg kan arbejde videre på stationær pc

--- app/views/js/cards.js
+++ app/views/js/cards.js
@@ -1,6 +1,41 @@
 $(() => {
+	$('div#set1').one('click', function () {
+		$(this).append(
+			`<div id="exercises">
+			<h2>Set 1</h2>
+			<p>E and Æ vowal sounds</p>
+		`
+		)
+		console.log('clicked')
+	})
 	$('div.details').on('click', function () {
 		$(this).toggleClass('clicked').css('transition-duration', '0.8s')
-		$('li .details').not('.clicked').toggleClass('.notSelected')
+		$('ul li .details').not('.clicked').toggleClass('.notSelected')
+	})
+
+	$('div#basic1').on('click', function () {
+		$(this).toggleClass('slideRight')
+	})
+
+	$('div#basic1').one('click', function () {
+		$('#container').prepend(
+			`<div class="smallcard" id="set1">
+		<h2>Set 1</h2>
+		<p>E and Æ vowal sounds</p>
+	</div>
+	<div class="smallcard">
+		<h2>Set 2</h2>
+		<p>Coming soon</p>
+	</div>
+	
+	<div class="smallcard">
+		<h2>Set 3</h2>
+		<p>Coming soon</p>
+	</div>
+	<div class="smallcard">
+		<h2>Set 4</h2>
+		<p>Coming soon</p>
+	</div>`
+		)
 	})
 })
