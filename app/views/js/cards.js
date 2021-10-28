5e1a60fd84026ecf78e5e2de354166bcafdef940
Sebastian <sechr17@student.sdu.dk>
2021-10-28 09:27:54 +0200

    more css(check description)
    så jeg kan arbejde videre på bærbar

--- app/views/js/cards.js
+++ app/views/js/cards.js
@@ -1,13 +1,4 @@
 $(() => {
-	$('div#set1').one('click', function () {
-		$(this).append(
-			`<div id="exercises">
-			<h2>Set 1</h2>
-			<p>E and Æ vowal sounds</p>
-		`
-		)
-		console.log('clicked')
-	})
 	$('div.details').on('click', function () {
 		$(this).toggleClass('clicked').css('transition-duration', '0.8s')
 		$('ul li .details').not('.clicked').toggleClass('.notSelected')
@@ -15,27 +6,31 @@ $(() => {
 
 	$('div#basic1').on('click', function () {
 		$(this).toggleClass('slideRight')
+		$('.smallcard').toggleClass('show')
 	})
 
-	$('div#basic1').one('click', function () {
+	$('div#set1').on('click', function () {
+		console.log('clicked')
+	})
+
+	/*$('div#basic1').one('click', function () {
 		$('#container').prepend(
 			`<div class="smallcard" id="set1">
 		<h2>Set 1</h2>
 		<p>E and Æ vowal sounds</p>
 	</div>
-	<div class="smallcard">
+	<div class="smallcard" id="set2">
 		<h2>Set 2</h2>
 		<p>Coming soon</p>
 	</div>
 	
-	<div class="smallcard">
+	<div class="smallcard" id="set3">
 		<h2>Set 3</h2>
 		<p>Coming soon</p>
 	</div>
-	<div class="smallcard">
+	<div class="smallcard" id="set4">
 		<h2>Set 4</h2>
 		<p>Coming soon</p>
-	</div>`
-		)
-	})
+	</div>
+	})*/
 })
