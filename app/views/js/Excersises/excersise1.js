272cdd22f4873b51ac9403c5aec2d67deea4965a
Michael Pedersen <micpe18@student.sdu.dk>
2021-10-30 14:47:02 +0200

    After drag return to center fix & refactoring

--- app/views/js/Excersises/excersise1.js
+++ app/views/js/Excersises/excersise1.js
@@ -12,10 +12,19 @@ library.add(faQuestionCircle);
 library.add(faVolumeUp);
 library.add(faTimes);
 
+/**
+ * Convertes from int to rem
+ * @param {Int} rem how many rem units
+ * @returns pixeles based of rem units
+ */
 function convertRemToPixels(rem) {
 	return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 }
-//TODO calculate position in context to where it is reletive to center x and y
+/**
+ * Finds the center of the mainContent class baseed of the cardstack position
+ * @param {String} card The target card div/class
+ * @returns returns x and y of maincontents div center is in gloabal space
+ */
 function findMaincontentCenter(card) {
 	let y = $(".mainContent").offset().top - convertRemToPixels(1);
 	let x = $(".mainContent").offset().left - convertRemToPixels(1);
@@ -27,6 +36,11 @@ function findMaincontentCenter(card) {
 }
 
 const timeline = anime.timeline;
+/**
+ * Animates from cardStack
+ * @param {String} card The target card div/class
+ * @returns Returns a pormis for when animation is done
+ */
 function animationFromStack(card) {
 	let maincontentCenter = findMaincontentCenter(card);
 
@@ -71,24 +85,27 @@ function animationFromStack(card) {
 
 	return t1.finished;
 }
-
+/**
+ * Used to animate back to where the card was draged from.
+ * @param {String} card The target card div/class
+ * @returns Returns a pormis for when animation is done
+ */
 function animationToCenter(card) {
-	let maincontentCenter = findMaincontentCenter(card);
-	position = maincontentCenter;
+	let { x, y } = position;
+	x = -x;
+	let animateTo = { x: x, y: y };
 	var t1 = timeline({
 		targets: card,
 	});
-	//TODO: Ændre slutning til at kortet ender centralt i "maincontent" div.
+
 	t1.add({
-		translateX: maincontentCenter.x,
-		translateY: maincontentCenter.x,
+		translateX: animateTo.x,
+		translateY: animateTo.y,
 		easing: "easeOutQuint",
 		duration: 1000,
 	});
-	position = {
-		x: maincontentCenter.x,
-		y: maincontentCenter.y,
-	};
+
+	position = { x: 0, y: 0 };
 
 	return t1.finished;
 }
@@ -98,29 +115,47 @@ let position = { x: 0, y: 0 };
 
 $(() => {
 	let draggable = false;
-	let offset = 5;
-	ord.forEach((element, index) => {
-		let card = `
-        <div class='cardcontainer cardcontainer${index}' id='cardcontainer${index}'>
-            <div class="card card${index}" id='card${index}'>
-                <div class="front">Bagside af kort</div>
-                <div class="back">${element}</div>
-            </div>
-        </div>`;
-		$(".cardStack-Container").append(card);
-		let zlayer = $(".front").css("z-index");
-		$(`#card${index}`).css({
-			transform: `translateX(${offset}px)`,
-			"z-index": zlayer + index,
-		});
-		offset += 5;
+	SetupHtmlDivs();
+
+	draggable = CardDraggable(draggable);
+
+	DropzoneCardInteract(".vokalE");
+	DropzoneCardInteract(".vokalÆ");
+
+	//Indsætning af ikon (krydset)
+	$(".close").append(icon({ prefix: "fas", iconName: "times" }).html);
+
+	$(".close svg").on("click", function () {
+		alert("Closing...");
+		//Afslut opgaven og gem fremskridt for at kunne fortsætte hvor man slap
 	});
 
-	let helpIcon = `<div class='helpIcon'>${
-		icon({ prefix: "fas", iconName: "question-circle" }).html
-	}</div>`;
-	$(".cardStack-Container").append(helpIcon);
+	ShowTutorialAgain();
+});
 
+function DropzoneCardInteract(div) {
+	interact(div).dropzone({
+		accept: ".card",
+		ondrop: function (event) {
+			alert("Hello");
+		},
+	});
+}
+
+/**
+ * The basic setup for the html document.
+ */
+function SetupHtmlDivs() {
+	MakeCardStack();
+
+	MakeHelpIcon();
+
+	MakeTutorialModal();
+}
+/**
+ * Makes the tutorial modal and its logical
+ */
+function MakeTutorialModal() {
 	$("#tutorialbutton").on("click", () => {
 		$(".tutorial").remove();
 		$("#tutorialbutton").remove();
@@ -128,7 +163,37 @@ $(() => {
 		$(".curtain").remove();
 		$(".speaker").append(icon({ prefix: "fas", iconName: "volume-up" }).html);
 	});
-	//TODO find a method to place the card in a container when the animation is done
+}
+
+/**
+ * Makes and inserts the help icon
+ */
+function MakeHelpIcon() {
+	let helpIcon = `<div class='helpIcon'>${
+		icon({ prefix: "fas", iconName: "question-circle" }).html
+	}</div>`;
+	$(".cardStack-Container").append(helpIcon);
+}
+
+/**
+ * Viser tutorial igen ved klik på hjælp ikon
+ */
+function ShowTutorialAgain() {
+	$(".helpIcon").on("click", function () {
+		$(".tutorial").css("visibility", "visible");
+		$("#tutorialbutton").css("visibility", "visible");
+		$("#drawcardContainer").css("visibility", "visible");
+		let curtain = `<div class="curtain"></div>`;
+		$("body").append(curtain);
+		$(".speaker").remove();
+	});
+}
+/**
+ *
+ * @param {Boolean} draggable Is the card draggable or not
+ * @returns Boolean depedendt if the card is draggable
+ */
+function CardDraggable(draggable) {
 	$("#drawCard").on("click", async () => {
 		animationFromStack(`.card5`)
 			.then(() => {
@@ -152,13 +217,7 @@ $(() => {
 							},
 						})
 						.on("dragend", (event) => {
-							animationToCenter(".card5").then(() => {
-								position = {
-									x: findMaincontentCenter(".card5").x,
-									y: findMaincontentCenter(".card5").y,
-								};
-								console.log(JSON.stringify(position));
-							});
+							animationToCenter(".card5");
 							$(".vokalE, .vokalÆ").css({
 								opacity: 1,
 								"border-style": "none",
@@ -170,38 +229,28 @@ $(() => {
 				console.log(error.message);
 			});
 	});
+	return draggable;
+}
 
-	interact(".vokalE").dropzone({
-		accept: ".card",
-		ondrop: function (event) {
-			alert("Hello");
-			//Rigtigt eller fokert svar
-		},
+/**
+ * Setsup the cardstack dependend on how many cards there is
+ */
+function MakeCardStack() {
+	let offset = 5;
+	ord.forEach((element, index) => {
+		let card = `
+        <div class='cardcontainer cardcontainer${index}' id='cardcontainer${index}'>
+            <div class="card card${index}" id='card${index}'>
+                <div class="front">Bagside af kort</div>
+                <div class="back">${element}</div>
+            </div>
+        </div>`;
+		$(".cardStack-Container").append(card);
+		let zlayer = $(".front").css("z-index");
+		$(`#card${index}`).css({
+			transform: `translateX(${offset}px)`,
+			"z-index": zlayer + index,
+		});
+		offset += 5;
 	});
-
-	interact(".vokalÆ").dropzone({
-		accept: ".card",
-		ondrop: function (event) {
-			alert("Hello");
-			//Rigtigt eller fokert svar
-		},
-	});
-
-	//Indsætning af ikon (krydset)
-	$(".close").append(icon({ prefix: "fas", iconName: "times" }).html);
-
-	$(".close svg").on("click", function () {
-		alert("Closing...");
-		//Afslut opgaven og gem fremskridt for at kunne fortsætte hvor man slap
-	});
-
-	//Viser tutorial igen ved klik på hjælp ikon
-	$(".helpIcon").on("click", function () {
-		$(".tutorial").css("visibility", "visible");
-		$("#tutorialbutton").css("visibility", "visible");
-		$("#drawcardContainer").css("visibility", "visible");
-		let curtain = `<div class="curtain"></div>`;
-		$("body").append(curtain);
-		$(".speaker").remove();
-	});
-});
+}
