cb83af3e1dee2f488a7064bdbbe9c479d26de70d
Sebastian <sechr17@student.sdu.dk>
2021-11-02 08:10:30 +0100

    more css(plus musik kek)

--- app/controller/routes/pages.js
+++ app/controller/routes/pages.js
@@ -17,4 +17,8 @@ router.route('/module-overview').get((req, res) => {
 	res.render('module-overview')
 })
 
+router.route('/exercise1').get((req, res) => {
+	res.render('exercise1')
+})
+
 module.exports = router
--- app/views/js/cards.js
+++ app/views/js/cards.js
@@ -5,43 +5,49 @@ $(() => {
 	})
 
 	$('div#basic1').on('click', function () {
+		const div = document.getElementById('exercise1')
+
 		$(this).toggleClass('slideRight')
 		$('.smallcard').toggleClass('show')
-		$('#basic2').css('visibility', 'hidden')
+		$('#basic2').toggleClass('hide')
+
+		$(div).addClass('hide')
+		$('.notselected').css('pointer-events', 'auto')
 	})
 
 	$('div#set1').one('click', function () {
-		console.log('clicked')
-		//transform: translate(5.5%, 15%) scale(1.1)
 		$(this).css('transform', 'translate(5.5%, 15%) scale(1.1)')
 		$(this).after(`
-		<div id="exercise1">
-		<div class="dashed-circle">Exercise 1</div>
-		<div class="dashed-circle">Exercise 2</div>
-		<div class="dashed-circle">Exercise 3</div>
-		<div class="dashed-circle">Exercise 4</div>
+		<div id="exercise1" class="hide">
+		<div class="dashed-circle exercise1">Exercise 1</div>
+		<div class="dashed-circle exercise2">Exercise 2</div>
+		<div class="dashed-circle exercise3">Exercise 3</div>
+		<div class="dashed-circle exercise4">Exercise 4</div>
+<div class="fourth-face dice test">
+				<div class="column">
+					<span class="dot"></span>
+					<span class="dot"></span>
+					<span class="dot"></span>
+					
+				</div>
+				<span style="color:white;font-size:40;text-align:center">Final test</span>
+				<div class="column">
+					<span class="dot"></span>
+					<span class="dot"></span>
+					<span class="dot"></span>
+				</div>
+			</div>
+
 		</div>
+		
 		`)
 	})
 
-	/*$('div#basic1').one('click', function () {
-		$('#container').prepend(
-			`<div class="smallcard" id="set1">
-		<h2>Set 1</h2>
-		<p>E and Æ vowal sounds</p>
-	</div>
-	<div class="smallcard" id="set2">
-		<h2>Set 2</h2>
-		<p>Coming soon</p>
-	</div>
-	
-	<div class="smallcard" id="set3">
-		<h2>Set 3</h2>
-		<p>Coming soon</p>
-	</div>
-	<div class="smallcard" id="set4">
-		<h2>Set 4</h2>
-		<p>Coming soon</p>
-	</div>
-	})*/
+	$('div#set1').on('click', function () {
+		console.log('clicked')
+		$('#exercise1').toggleClass('hide')
+		$('.exercise1').on('click', function () {
+			window.open('http://localhost:3000/page/exercise1', '_parent')
+		})
+	})
 })
