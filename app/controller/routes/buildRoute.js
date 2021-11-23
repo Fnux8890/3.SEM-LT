84d9dce62e81ba7c3298cb564d8dddc0e33c5226
Jacob Florczak <jaflo18@student.sdu.dk>
2021-11-23 12:03:52 +0100

    Beginning Test

--- app/controller/routes/buildRoute.js
+++ app/controller/routes/buildRoute.js
@@ -11,278 +11,281 @@ const binary = mongodb.Binary;
 const mongoClient = mongodb.MongoClient;
 
 router.post("/postRecording", (req, res) => {
-	let recording = {
-		name: req.body.name,
-		file: binary(req.files.uploadedFile.data),
-	};
-	insertRecording(recording, res);
-	console.log(recording);
-	res.end();
+  let recording = {
+    name: req.body.name,
+    file: binary(req.files.uploadedFile.data),
+  };
+  insertRecording(recording, res);
+  console.log(recording);
+  res.end();
 });
 // ? Not surtain what this does
 router.post("/postSentence", (req, res) => {
-	let sentence = {
-		name: req.body.name,
-		file: binary(req.files.uploadedFile.data),
-	};
-	insertSentence(sentence, res);
-	console.log(sentence);
-	res.end();
+  let sentence = {
+    name: req.body.name,
+    file: binary(req.files.uploadedFile.data),
+  };
+  insertSentence(sentence, res);
+  console.log(sentence);
+  res.end();
 });
 //TODO rewrite to use mongodb instead of having to connections to mongodb
 function insertRecording(recording, res) {
-	const url =
-		"mongodb+srv://Sebastian:warrcraft1@cluster0.op3ym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const url =
+    "mongodb+srv://Sebastian:warrcraft1@cluster0.op3ym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-	mongoClient.connect(url, (err, client) => {
-		if (err) {
-			return err;
-		} else {
-			try {
-				wordModel
-					.findOneAndUpdate({ word: recording.name }, { soundfile: recording })
-					.exec();
-				console.log("inserted recording");
-			} catch (err) {
-				console.log(err.message);
-				console.log("err while inserting");
-			}
-		}
-	});
+  mongoClient.connect(url, (err, client) => {
+    if (err) {
+      return err;
+    } else {
+      try {
+        wordModel
+          .findOneAndUpdate(
+            { word: recording.name },
+            { $push: { soundfile: recording.file } }
+          )
+          .exec();
+        console.log("inserted recording");
+      } catch (err) {
+        console.log(err.message);
+        console.log("err while inserting");
+      }
+    }
+  });
 }
 
 function insertSentence(sentence, res) {
-	const url =
-		"mongodb+srv://Sebastian:warrcraft1@cluster0.op3ym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const url =
+    "mongodb+srv://Sebastian:warrcraft1@cluster0.op3ym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-	mongoClient.connect(url, (err, client) => {
-		if (err) {
-			return err;
-		} else {
-			try {
-				sentenceModel
-					.findOneAndUpdate(
-						{ sentence: sentence.name },
-						{ soundfile: sentence }
-					)
-					.exec();
-				console.log("inserted recording");
-			} catch (err) {
-				console.log(err.message);
-				console.log("err while inserting");
-			}
-		}
-	});
+  mongoClient.connect(url, (err, client) => {
+    if (err) {
+      return err;
+    } else {
+      try {
+        sentenceModel
+          .findOneAndUpdate(
+            { sentence: sentence.name },
+            { $push: { soundfile: sentence.file } }
+          )
+          .exec();
+        console.log("inserted recording");
+      } catch (err) {
+        console.log(err.message);
+        console.log("err while inserting");
+      }
+    }
+  });
 }
 
 async function getExerciseWithWords(id) {
-	let result = await exercisesModel.aggregate([
-		{
-			$match: {
-				name: `Exercise ${id}`,
-			},
-		},
-		{
-			$unwind: {
-				path: "$cards",
-			},
-		},
-		{
-			$addFields: {
-				cards: {
-					wordId: {
-						$toObjectId: "$cards.wordId",
-					},
-					sentenceId: {
-						$toObjectId: "$cards.sentenceId",
-					},
-				},
-			},
-		},
-		{
-			$lookup: {
-				from: "words",
-				localField: "cards.wordId",
-				foreignField: "_id",
-				as: "cards.word",
-			},
-		},
-		{
-			$unwind: {
-				path: "$cards.word",
-			},
-		},
-		{
-			$unwind: {
-				path: "$cards.word.soundfile",
-			},
-		},
-		{
-			$project: {
-				_id: 0,
-				name: "$name",
-				description: "$description",
-				subject: "$subject",
-				instructions: "$instructions",
-				answerOptions: "$answerOptions",
-				cards: {
-					word: "$cards.word.word",
-					translation_word: "$cards.word.translation",
-					soundfile_word: "$cards.word.soundfile.file",
-					answer: "$cards.answer",
-					sentence: "$cards.sentence.sentence",
-					translation_sentence: "$cards.sentence.translation",
-					soundfile_sentence: "$cards.sentence.soundfile.file",
-				},
-			},
-		},
-		{
-			$group: {
-				_id: {
-					name: "$name",
-					description: "$description",
-					instructions: "$instructions",
-					subject: "$subject",
-					answerOptions: "$answerOptions",
-				},
-				cards: {
-					$addToSet: "$cards",
-				},
-			},
-		},
-		{
-			$project: {
-				_id: 0,
-				name: "$_id.name",
-				description: "$_id.description",
-				subject: "$_id.subject",
-				instructions: "$_id.instructions",
-				answerOptions: "$_id.answerOptions",
-				cards: "$cards",
-			},
-		},
-	]);
-	return result[0];
+  let result = await exercisesModel.aggregate([
+    {
+      $match: {
+        name: `Exercise ${id}`,
+      },
+    },
+    {
+      $unwind: {
+        path: "$cards",
+      },
+    },
+    {
+      $addFields: {
+        cards: {
+          wordId: {
+            $toObjectId: "$cards.wordId",
+          },
+          sentenceId: {
+            $toObjectId: "$cards.sentenceId",
+          },
+        },
+      },
+    },
+    {
+      $lookup: {
+        from: "words",
+        localField: "cards.wordId",
+        foreignField: "_id",
+        as: "cards.word",
+      },
+    },
+    {
+      $unwind: {
+        path: "$cards.word",
+      },
+    },
+    {
+      $unwind: {
+        path: "$cards.word.soundfile",
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        name: "$name",
+        description: "$description",
+        subject: "$subject",
+        instructions: "$instructions",
+        answerOptions: "$answerOptions",
+        cards: {
+          word: "$cards.word.word",
+          translation_word: "$cards.word.translation",
+          soundfile_word: "$cards.word.soundfile.file",
+          answer: "$cards.answer",
+          sentence: "$cards.sentence.sentence",
+          translation_sentence: "$cards.sentence.translation",
+          soundfile_sentence: "$cards.sentence.soundfile.file",
+        },
+      },
+    },
+    {
+      $group: {
+        _id: {
+          name: "$name",
+          description: "$description",
+          instructions: "$instructions",
+          subject: "$subject",
+          answerOptions: "$answerOptions",
+        },
+        cards: {
+          $addToSet: "$cards",
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        name: "$_id.name",
+        description: "$_id.description",
+        subject: "$_id.subject",
+        instructions: "$_id.instructions",
+        answerOptions: "$_id.answerOptions",
+        cards: "$cards",
+      },
+    },
+  ]);
+  return result[0];
 }
 
-async function getExerciseSentences() {
-	let result = await exercisesModel.aggregate([
-		{
-			$match: {
-				name: "Exercise 2",
-			},
-		},
-		{
-			$unwind: {
-				path: "$cards",
-			},
-		},
-		{
-			$addFields: {
-				cards: {
-					wordId: {
-						$toObjectId: "$cards.wordId",
-					},
-					sentenceId: {
-						$toObjectId: "$cards.sentenceId",
-					},
-				},
-			},
-		},
-		{
-			$lookup: {
-				from: "words",
-				localField: "cards.wordId",
-				foreignField: "_id",
-				as: "cards.word",
-			},
-		},
-		{
-			$lookup: {
-				from: "sentences",
-				localField: "cards.sentenceId",
-				foreignField: "_id",
-				as: "cards.sentence",
-			},
-		},
-		{
-			$unwind: {
-				path: "$cards.word",
-			},
-		},
-		{
-			$unwind: {
-				path: "$cards.sentence",
-			},
-		},
-		{
-			$unwind: {
-				path: "$cards.word.soundfile",
-			},
-		},
-		{
-			$unwind: {
-				path: "$cards.sentence.soundfile",
-			},
-		},
-		{
-			$project: {
-				_id: 0,
-				name: "$name",
-				description: "$description",
-				subject: "$subject",
-				instructions: "$instructions",
-				cards: {
-					word: "$cards.word.word",
-					translation_word: "$cards.word.translation",
-					soundfile_word: "$cards.word.soundfile.file",
-					sentence: "$cards.sentence.sentence",
-					translation_sentence: "$cards.sentence.translation",
-					soundfile_sentence: "$cards.sentence.soundfile.file",
-				},
-			},
-		},
-		{
-			$group: {
-				_id: {
-					name: "$name",
-					description: "$description",
-					instructions: "$instructions",
-					subject: "$subject",
-					answerOptions: "$answerOptions",
-				},
-				cards: {
-					$addToSet: "$cards",
-				},
-			},
-		},
-		{
-			$project: {
-				_id: 0,
-				name: "$_id.name",
-				description: "$_id.description",
-				subject: "$_id.subject",
-				instructions: "$_id.instructions",
-				answerOptions: "$_id.answerOptions",
-				cards: "$cards",
-			},
-		},
-	]);
-	return result[0];
+async function ExerciseSentences() {
+  let result = await exercisesModel.aggregate([
+    {
+      $match: {
+        name: "Exercise 2",
+      },
+    },
+    {
+      $unwind: {
+        path: "$cards",
+      },
+    },
+    {
+      $addFields: {
+        cards: {
+          wordId: {
+            $toObjectId: "$cards.wordId",
+          },
+          sentenceId: {
+            $toObjectId: "$cards.sentenceId",
+          },
+        },
+      },
+    },
+    {
+      $lookup: {
+        from: "words",
+        localField: "cards.wordId",
+        foreignField: "_id",
+        as: "cards.word",
+      },
+    },
+    {
+      $lookup: {
+        from: "sentences",
+        localField: "cards.sentenceId",
+        foreignField: "_id",
+        as: "cards.sentence",
+      },
+    },
+    {
+      $unwind: {
+        path: "$cards.word",
+      },
+    },
+    {
+      $unwind: {
+        path: "$cards.sentence",
+      },
+    },
+    {
+      $unwind: {
+        path: "$cards.word.soundfile",
+      },
+    },
+    {
+      $unwind: {
+        path: "$cards.sentence.soundfile",
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        name: "$name",
+        description: "$description",
+        subject: "$subject",
+        instructions: "$instructions",
+        cards: {
+          word: "$cards.word.word",
+          translation_word: "$cards.word.translation",
+          soundfile_word: "$cards.word.soundfile.file",
+          sentence: "$cards.sentence.sentence",
+          translation_sentence: "$cards.sentence.translation",
+          soundfile_sentence: "$cards.sentence.soundfile.file",
+        },
+      },
+    },
+    {
+      $group: {
+        _id: {
+          name: "$name",
+          description: "$description",
+          instructions: "$instructions",
+          subject: "$subject",
+          answerOptions: "$answerOptions",
+        },
+        cards: {
+          $addToSet: "$cards",
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        name: "$_id.name",
+        description: "$_id.description",
+        subject: "$_id.subject",
+        instructions: "$_id.instructions",
+        answerOptions: "$_id.answerOptions",
+        cards: "$cards",
+      },
+    },
+  ]);
+  return result[0];
 }
 
 router.route("/ExerciseWords").get(async (req, res) => {
-	if (req.query.id === undefined) {
-		res.json({ error: "No id given" });
-		return;
-	}
-	let exerciseObj = await getExerciseWithWords(req.query.id);
-	res.json(exerciseObj);
+  if (req.query.id === undefined) {
+    res.json({ error: "No id given" });
+    return;
+  }
+  let exerciseObj = await getExerciseWithWords(req.query.id);
+  res.json(exerciseObj);
 });
 
 router.route("/ExerciseWordsAndSentences").get(async (req, res) => {
-	let sentenceQuery = await getExerciseSentences();
-	res.json(sentenceQuery);
+  let sentenceQuery = await getExerciseSentences();
+  res.json(sentenceQuery);
 });
 
 export default router;
