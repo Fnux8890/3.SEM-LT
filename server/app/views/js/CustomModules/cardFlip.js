d6e7d288551830a51c60a312d1f7c7cc5877d43a
Michael Pedersen <micpe18@student.sdu.dk>
2021-11-23 13:47:13 +0100

    some structure changes for heroku

--- server/app/views/js/CustomModules/cardFlip.js
+++ server/app/views/js/CustomModules/cardFlip.js
@@ -1,5 +1,5 @@
 import anime from "animejs";
-import { default as audioPlayer } from "../CustomModules/audioPlayer";
+import { default as audioPlayer } from "./audioPlayer";
 export function cardFlip(card, currentCard) {
 	let cardFlip = anime.timeline({
 		targets: card,
