cf737763049d6ffc68ff99ddd006f8319dd9c8bf
Sebastian <sechr17@student.sdu.dk>
2021-11-24 09:26:10 +0100

    ændret css

--- app/controller/routes/buildRoute.js
+++ app/controller/routes/buildRoute.js
@@ -1,16 +1,16 @@
-import { Router } from "express";
-import mongoose from "mongoose";
-import setModel from "../../models/setModel";
-import exercisesModel from "../../models/exercisesModel";
-import wordModel from "../../models/wordsModel";
-import sentenceModel from "../../models/sentenceModel";
+import { Router } from 'express';
+import mongoose from 'mongoose';
+import setModel from '../../models/setModel';
+import exercisesModel from '../../models/exercisesModel';
+import wordModel from '../../models/wordsModel';
+import sentenceModel from '../../models/sentenceModel';
 const router = Router();
-const mongodb = require("mongodb");
+const mongodb = require('mongodb');
 
 const binary = mongodb.Binary;
 const mongoClient = mongodb.MongoClient;
 
-router.post("/postRecording", (req, res) => {
+router.post('/postRecording', (req, res) => {
 	let recording = {
 		name: req.body.name,
 		file: binary(req.files.uploadedFile.data),
@@ -19,8 +19,8 @@ router.post("/postRecording", (req, res) => {
 	console.log(recording);
 	res.end();
 });
-// ? Not surtain what this does
-router.post("/postSentence", (req, res) => {
+// ? Not surtain what this does: svar: poster en sentence til mongodb
+router.post('/postSentence', (req, res) => {
 	let sentence = {
 		name: req.body.name,
 		file: binary(req.files.uploadedFile.data),
@@ -32,7 +32,7 @@ router.post("/postSentence", (req, res) => {
 //TODO rewrite to use mongodb instead of having to connections to mongodb
 function insertRecording(recording, res) {
 	const url =
-		"mongodb+srv://Sebastian:warrcraft1@cluster0.op3ym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+		'mongodb+srv://Sebastian:warrcraft1@cluster0.op3ym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 	mongoClient.connect(url, (err, client) => {
 		if (err) {
@@ -40,12 +40,15 @@ function insertRecording(recording, res) {
 		} else {
 			try {
 				wordModel
-					.findOneAndUpdate({ word: recording.name }, { soundfile: recording })
+					.findOneAndUpdate(
+						{ word: recording.name },
+						{ soundfile: recording }
+					)
 					.exec();
-				console.log("inserted recording");
+				console.log('inserted recording');
 			} catch (err) {
 				console.log(err.message);
-				console.log("err while inserting");
+				console.log('err while inserting');
 			}
 		}
 	});
@@ -53,7 +56,7 @@ function insertRecording(recording, res) {
 
 function insertSentence(sentence, res) {
 	const url =
-		"mongodb+srv://Sebastian:warrcraft1@cluster0.op3ym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+		'mongodb+srv://Sebastian:warrcraft1@cluster0.op3ym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 	mongoClient.connect(url, (err, client) => {
 		if (err) {
@@ -66,10 +69,10 @@ function insertSentence(sentence, res) {
 						{ soundfile: sentence }
 					)
 					.exec();
-				console.log("inserted recording");
+				console.log('inserted recording');
 			} catch (err) {
 				console.log(err.message);
-				console.log("err while inserting");
+				console.log('err while inserting');
 			}
 		}
 	});
@@ -84,81 +87,81 @@ async function getExerciseWithWords(id) {
 		},
 		{
 			$unwind: {
-				path: "$cards",
+				path: '$cards',
 			},
 		},
 		{
 			$addFields: {
 				cards: {
 					wordId: {
-						$toObjectId: "$cards.wordId",
+						$toObjectId: '$cards.wordId',
 					},
 					sentenceId: {
-						$toObjectId: "$cards.sentenceId",
+						$toObjectId: '$cards.sentenceId',
 					},
 				},
 			},
 		},
 		{
 			$lookup: {
-				from: "words",
-				localField: "cards.wordId",
-				foreignField: "_id",
-				as: "cards.word",
+				from: 'words',
+				localField: 'cards.wordId',
+				foreignField: '_id',
+				as: 'cards.word',
 			},
 		},
 		{
 			$unwind: {
-				path: "$cards.word",
+				path: '$cards.word',
 			},
 		},
 		{
 			$unwind: {
-				path: "$cards.word.soundfile",
+				path: '$cards.word.soundfile',
 			},
 		},
 		{
 			$project: {
 				_id: 0,
-				name: "$name",
-				description: "$description",
-				subject: "$subject",
-				instructions: "$instructions",
-				answerOptions: "$answerOptions",
+				name: '$name',
+				description: '$description',
+				subject: '$subject',
+				instructions: '$instructions',
+				answerOptions: '$answerOptions',
 				cards: {
-					word: "$cards.word.word",
-					translation_word: "$cards.word.translation",
-					soundfile_word: "$cards.word.soundfile.file",
-					answer: "$cards.answer",
-					sentence: "$cards.sentence.sentence",
-					translation_sentence: "$cards.sentence.translation",
-					soundfile_sentence: "$cards.sentence.soundfile.file",
+					word: '$cards.word.word',
+					translation_word: '$cards.word.translation',
+					soundfile_word: '$cards.word.soundfile.file',
+					answer: '$cards.answer',
+					sentence: '$cards.sentence.sentence',
+					translation_sentence: '$cards.sentence.translation',
+					soundfile_sentence: '$cards.sentence.soundfile.file',
 				},
 			},
 		},
 		{
 			$group: {
 				_id: {
-					name: "$name",
-					description: "$description",
-					instructions: "$instructions",
-					subject: "$subject",
-					answerOptions: "$answerOptions",
+					name: '$name',
+					description: '$description',
+					instructions: '$instructions',
+					subject: '$subject',
+					answerOptions: '$answerOptions',
 				},
 				cards: {
-					$addToSet: "$cards",
+					$addToSet: '$cards',
 				},
 			},
 		},
 		{
 			$project: {
 				_id: 0,
-				name: "$_id.name",
-				description: "$_id.description",
-				subject: "$_id.subject",
-				instructions: "$_id.instructions",
-				answerOptions: "$_id.answerOptions",
-				cards: "$cards",
+				name: '$_id.name',
+				description: '$_id.description',
+				subject: '$_id.subject',
+				instructions: '$_id.instructions',
+				answerOptions: '$_id.answerOptions',
+				cards: '$cards',
 			},
 		},
 	]);
@@ -169,118 +172,118 @@ async function getExerciseSentences() {
 	let result = await exercisesModel.aggregate([
 		{
 			$match: {
-				name: "Exercise 2",
+				name: 'Exercise 2',
 			},
 		},
 		{
 			$unwind: {
-				path: "$cards",
+				path: '$cards',
 			},
 		},
 		{
 			$addFields: {
 				cards: {
 					wordId: {
-						$toObjectId: "$cards.wordId",
+						$toObjectId: '$cards.wordId',
 					},
 					sentenceId: {
-						$toObjectId: "$cards.sentenceId",
+						$toObjectId: '$cards.sentenceId',
 					},
 				},
 			},
 		},
 		{
 			$lookup: {
-				from: "words",
-				localField: "cards.wordId",
-				foreignField: "_id",
-				as: "cards.word",
+				from: 'words',
+				localField: 'cards.wordId',
+				foreignField: '_id',
+				as: 'cards.word',
 			},
 		},
 		{
 			$lookup: {
-				from: "sentences",
-				localField: "cards.sentenceId",
-				foreignField: "_id",
-				as: "cards.sentence",
+				from: 'sentences',
+				localField: 'cards.sentenceId',
+				foreignField: '_id',
+				as: 'cards.sentence',
 			},
 		},
 		{
 			$unwind: {
-				path: "$cards.word",
+				path: '$cards.word',
 			},
 		},
 		{
 			$unwind: {
-				path: "$cards.sentence",
+				path: '$cards.sentence',
 			},
 		},
 		{
 			$unwind: {
-				path: "$cards.word.soundfile",
+				path: '$cards.word.soundfile',
 			},
 		},
 		{
 			$unwind: {
-				path: "$cards.sentence.soundfile",
+				path: '$cards.sentence.soundfile',
 			},
 		},
 		{
 			$project: {
 				_id: 0,
-				name: "$name",
-				description: "$description",
-				subject: "$subject",
-				instructions: "$instructions",
+				name: '$name',
+				description: '$description',
+				subject: '$subject',
+				instructions: '$instructions',
 				cards: {
-					word: "$cards.word.word",
-					translation_word: "$cards.word.translation",
-					soundfile_word: "$cards.word.soundfile.file",
-					sentence: "$cards.sentence.sentence",
-					translation_sentence: "$cards.sentence.translation",
-					soundfile_sentence: "$cards.sentence.soundfile.file",
+					word: '$cards.word.word',
+					translation_word: '$cards.word.translation',
+					soundfile_word: '$cards.word.soundfile.file',
+					sentence: '$cards.sentence.sentence',
+					translation_sentence: '$cards.sentence.translation',
+					soundfile_sentence: '$cards.sentence.soundfile.file',
 				},
 			},
 		},
 		{
 			$group: {
 				_id: {
-					name: "$name",
-					description: "$description",
-					instructions: "$instructions",
-					subject: "$subject",
-					answerOptions: "$answerOptions",
+					name: '$name',
+					description: '$description',
+					instructions: '$instructions',
+					subject: '$subject',
+					answerOptions: '$answerOptions',
 				},
 				cards: {
-					$addToSet: "$cards",
+					$addToSet: '$cards',
 				},
 			},
 		},
 		{
 			$project: {
 				_id: 0,
-				name: "$_id.name",
-				description: "$_id.description",
-				subject: "$_id.subject",
-				instructions: "$_id.instructions",
-				answerOptions: "$_id.answerOptions",
-				cards: "$cards",
+				name: '$_id.name',
+				description: '$_id.description',
+				subject: '$_id.subject',
+				instructions: '$_id.instructions',
+				answerOptions: '$_id.answerOptions',
+				cards: '$cards',
 			},
 		},
 	]);
 	return result[0];
 }
 
-router.route("/ExerciseWords").get(async (req, res) => {
+router.route('/ExerciseWords').get(async (req, res) => {
 	if (req.query.id === undefined) {
-		res.json({ error: "No id given" });
+		res.json({ error: 'No id given' });
 		return;
 	}
 	let exerciseObj = await getExerciseWithWords(req.query.id);
 	res.json(exerciseObj);
 });
 
-router.route("/ExerciseWordsAndSentences").get(async (req, res) => {
+router.route('/ExerciseWordsAndSentences').get(async (req, res) => {
 	let sentenceQuery = await getExerciseSentences();
 	res.json(sentenceQuery);
 });
--- app/views/js/moduleOverview.js
+++ app/views/js/moduleOverview.js
@@ -1,30 +1,32 @@
-import "../assets/scss/layouts/modules.scss";
+import '../assets/scss/layouts/modules.scss';
 
 $(() => {
-	$("div.details").on("click", function () {
-		$(this).toggleClass("clicked").css("transition-duration", "0.8s");
-		$("ul li .details").not(".clicked").toggleClass(".notSelected");
+	$('div.details').on('click', function () {
+		$(this).toggleClass('clicked').css('transition-duration', '0.8s');
+		$('ul li .details').not('.clicked').toggleClass('.notSelected');
 	});
 
-	$("div#basic1").on("click", function () {
-		const div = document.getElementById("exercise1");
+	$('div#basic1').on('click', function () {
+		const div = document.getElementById('exercise1');
 
-		$(this).toggleClass("slideRight");
-		$(".smallcard").toggleClass("show");
-		$("#basic2").toggleClass("hide");
+		$(this).toggleClass('slideRight');
+		$('.smallcard').toggleClass('show');
+		$('#basic2').toggleClass('hide');
+		$('#basic3').toggleClass('hide');
+		$('#basic4').toggleClass('hide');
+		$('#special1').toggleClass('hide');
 
-		$(div).addClass("hide");
-		$(".notselected").css("pointer-events", "auto");
+		$(div).addClass('hide');
+		$('.notselected').css('pointer-events', 'auto');
 	});
 
-	$("div#set1").one("click", function () {
-		$(this).css("transform", "translate(5.5%, 15%) scale(1.1)");
+	$('div#set1').one('click', function () {
+		$(this).css('transform', 'translate(5.5%, 15%) scale(1.1)');
 		$(this).after(`
 		<div id="exercise1" class="hide">
 		<div class="dashed-circle exercise1">Exercise 1</div>
 		<div class="dashed-circle exercise2">Exercise 2</div>
 		<div class="dashed-circle exercise3">Exercise 3</div>
-		<div class="dashed-circle exercise4">Exercise 4</div>
 <div class="fourth-face dice test">
 				<div class="column">
 					<span class="dot"></span>
@@ -45,14 +47,17 @@ $(() => {
 		`);
 	});
 
-	$("div#set1").on("click", function () {
-		console.log("clicked");
-		$("#exercise1").toggleClass("hide");
-		$(".exercise1").on("click", function () {
-			window.open("http://localhost:3000/page/exercise1", "_parent");
+	$('div#set1').on('click', function () {
+		console.log('clicked');
+		$('#exercise1').toggleClass('hide');
+		$('.exercise1').on('click', function () {
+			window.open('http://localhost:3000/page/exercise1', '_parent');
 		});
-		$(".exercise2").on("click", function () {
-			window.open("http://localhost:3000/page/exercise2", "_parent");
+		$('.exercise2').on('click', function () {
+			window.open('http://localhost:3000/page/exercise2', '_parent');
+		});
+		$('.exercise3').on('click', function () {
+			window.open('http://localhost:3000/page/exercise3', '_parent');
 		});
 	});
 });
