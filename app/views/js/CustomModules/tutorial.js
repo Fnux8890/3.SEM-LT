f8fd48bac7ed017bf28cb6215436e6f1497b60cc
Michael Pedersen <micpe18@student.sdu.dk>
2021-11-26 14:28:50 +0100

    tutorial fix setTest

--- app/views/js/CustomModules/tutorial.js
+++ app/views/js/CustomModules/tutorial.js
@@ -1,8 +1,13 @@
 export function populateTutorial(data) {
 	let eng = `<h3>English instructions</h3>`;
 	let dan = `<h3>Danish instructions</h3>`;
-	$('#eng .text').html(`${data.instructions.instructionsENG}`);
-	$('#dan .text').html(`${data.instructions.instructionsDK}`);
+	if (Array.isArray(data.instructions)) {
+		$('#eng .text').html(`${data.instructions[0].instructionsENG}`);
+		$('#dan .text').html(`${data.instructions[0].instructionsDK}`);
+	} else {
+		$('#eng .text').html(`${data.instructions.instructionsENG}`);
+		$('#dan .text').html(`${data.instructions.instructionsDK}`);
+	}
 	$('#eng .title').html(eng);
 	$('#dan .title').html(dan);
 }
