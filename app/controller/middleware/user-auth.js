43658dd17243712d0f43d57e284606cbbb983d95
Sebastian <sechr17@student.sdu.dk>
2021-12-03 11:24:52 +0100

    små fixes

--- app/controller/middleware/user-auth.js
+++ app/controller/middleware/user-auth.js
@@ -1,17 +1,17 @@
-import { verify } from "jsonwebtoken";
+import { verify } from 'jsonwebtoken';
 
 export const requireAuth = (req, res, next) => {
-  const token = req.cookies.jwt;
+	const token = req.cookies.jwt;
 
-  if (token) {
-    verify(token, process.env.JWT_KEY, (err, decodedToken) => {
-      if (err) {
-        res.redirect('/page/login');
-      } else {
-        next();
-      }
-    });
-  } else {
-    res.redirect('/page/login');
-  }
+	if (token) {
+		verify(token, process.env.JWT_KEY, (err, decodedToken) => {
+			if (err) {
+				res.redirect('/page/login');
+			} else {
+				next();
+			}
+		});
+	} else {
+		res.redirect('/page/login');
+	}
 };
